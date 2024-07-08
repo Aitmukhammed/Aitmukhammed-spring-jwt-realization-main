9c79c6668c09d6f6471a4a3bf619115870cc8e61
Aitmukhammed <aitmukhammed.04@gmail.com>
2024-07-08 15:56:53 +0500

    Добавил поле - Category, но честно забыл почему

--- spring-jwt-realization-main/src/main/java/com/example/demoauth/DTO/ProductDTO.java
+++ spring-jwt-realization-main/src/main/java/com/example/demoauth/DTO/ProductDTO.java
@@ -1,40 +1,42 @@
 package com.example.demoauth.DTO;
 
+import com.example.demoauth.models.Category;
+
 public class ProductDTO {
     private Long id;
     private String name;
     private double price;
     private String pictureUrl;
+    private Category category;
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
     public String getPictureUrl() {
         return pictureUrl;
     }
-
     public void setPictureUrl(String pictureUrl) {
         this.pictureUrl = pictureUrl;
+    }
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
     }
 }
