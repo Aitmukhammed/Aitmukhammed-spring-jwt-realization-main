2f6cd2f8b45ae29de5f4e93f9b77b48f46876c69
Aitmukhammed <aitmukhammed.04@gmail.com>
2024-07-08 15:51:15 +0500

    Написал метод - findAllOrderByCategoryNameDesc = который выводить всех пробуктов по убыванию букв названии(name)

--- spring-jwt-realization-main/src/main/java/com/example/demoauth/repository/ProductRepository.java
+++ spring-jwt-realization-main/src/main/java/com/example/demoauth/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.example.demoauth.repository;
 
+import com.example.demoauth.models.Category;
 import com.example.demoauth.models.Product;
 import com.example.demoauth.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,7 +17,14 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findById(Long id);
 
+//@Query("SELECT p FROM Product p JOIN p.category c ORDER BY c.name DESC")
+    @Query("SELECT p FROM Product p LEFT JOIN p.category c ORDER BY c.name DESC")
+    List<Product> findAllOrderByCategoryNameDesc();
+
     List<Product> findByCategoryId(Long categoryId);
 
     List<Product> findByCategoryName(String categoryName);
+
+    List<Product> findByCategoryCategoryCode(Long categoryCode);
+
 }
